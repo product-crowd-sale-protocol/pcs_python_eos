3b887461f7dc4e1629b3f80576f44291340574ea
leohio <leonahioki@gmail.com>
2019-03-13 01:25:23 +0900

    dexclient.py: all methods are written as in EOS contract

--- dexclient.py
+++ dexclient.py
@@ -1,6 +1,7 @@
-#DEX
+from eospy.eos_client import EosClient
 
 class DEXManager(EosClient):
+
     def addsellobyid(self,sym,token_id,price,memo):
         binargs = self.chain_abi_json_to_bin({
             "code": CONTRACT, "action": "create",
@@ -12,12 +13,93 @@ class DEXManager(EosClient):
         ))
         return self.push_transaction(transaction, chain_id)
             
-    def call_buyfromorder():
-    def call_cancelsobyid():
-    def call_cancelsello():
-    def call_addbuyorder():
-    def call_selltoorder():
-    def call_cancelbobyid():
-    def call_cancelbuyo():
-    def call_withdraw():
+    def buyfromorder(self, buyer, symbol, token_id, memo ):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "buyfromorder",
+            "args": {"sym": symbol,"token_id":token_id,"memo":memo}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'buyfromorder',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
+    def cancelsobyid(self,symbol, token_id):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "cancelsobyid",
+            "args": {"sym": symbol,"token_id":token_id}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'cancelsobyid',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+    
+    #quantity is assset type (like "100 EOS")       
+    def cancelsello(self,quantity):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "cancelsello",
+            "args": {"seller": self.account,"quantity":quantity}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'cancelsello',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
+    def addbuyorder(self,symbol,price,memo):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "addbuyorder",
+            "args": {"buyer":self.account,"sym": symbol,"price":price,"memo":memo}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'addbuyorder',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
+    def selltoorder(self,symbol,token_id, order_id, memo):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "selltoorder",
+            "args": {"sym": symbol,"token_id":token_id,"order_id":order_id,"memo":memo}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'selltoorder',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
+    def cancelbobyid(self,symbol, order_id):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "cancelbobyid",
+            "args": {"sym": symbol,"order_id":token_id}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'cancelbobyid',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
+    def cancelbuyo(self, quantity):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "cancelbuyo",
+            "args": {"buyer": self.account,"quantity":quantity}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'cancelbuyo',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+    #value is asset type
+    def withdraw(self,value, memo):
+        binargs = self.chain_abi_json_to_bin({
+            "code": CONTRACT, "action": "withdraw",
+            "args": {"user":self.account,"value":value,"memo":memo}
+        })['binargs']
+
+        transaction, chain_id = TransactionBuilder(self).build_sign_transaction_request((
+            Action(CONTRACT, 'withdraw',self.account , self.permission, transfer_binargs),
+        ))
+        return self.push_transaction(transaction, chain_id)
+            
     
