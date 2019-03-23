2c66ef13190dc9d76af972140db5c35cefed7849
CoRe103 <hiruma@geomerlin.com>
2019-03-24 03:14:28 +0900

    add: sig message for lock and transferid2

--- client.py
+++ client.py
@@ -3,11 +3,11 @@ import datetime
 from eospy.eos_client import EosClient
 from eospy import table_client
 from check_sig import check_sig ,generate_key
-from eospy.endpoints import CONTRACT 
+from eospy.endpoints import CONTRACT
 from eospy.transaction_builder import TransactionBuilder, Action
 
 class PCSClient(EosClient):
-   
+
     def check_security(self,symbol,tokenId):
         return check_sig.check_sig(symbol,tokenId,self.subprivatekey)
 
@@ -17,11 +17,11 @@ class PCSClient(EosClient):
 
     def set_keys_by_password(self,password,symbol,tokenId=False):
 
-        #False is flag of automation. get next id automatically 
+        #False is flag of automation. get next id automatically
         if tokenId==False :
             tokenId = table_client.get_token_lastid(symbol)
             print("Given ID of {0} is {1}".format(symbol,tokenId+1))
-             
+
         if tokenId<-1:
             raise
 
@@ -30,7 +30,7 @@ class PCSClient(EosClient):
         print(prvkey)
         self.subkey=pubkey
         self.subprivatekey= prvkey
-                  
+
     def create(self,symbol):
 
         binargs = self.chain_abi_json_to_bin({
@@ -56,12 +56,12 @@ class PCSClient(EosClient):
         ))
 
         return self.push_transaction(transaction, chain_id)
-    
+
     def transferbyid(self, to , symbol ,token_id , memo ):
 
         binargs = self.chain_abi_json_to_bin({
             "code": CONTRACT, "action": "transferbyid",
-            "args": {"from": self.account, 
+            "args": {"from": self.account,
                      "to": to,
                      "sym": symbol,
                      "token_id":token_id,
@@ -75,7 +75,7 @@ class PCSClient(EosClient):
 
 
     def refreshkey(self,symbol,token_id,new_subkey):
-    
+
         binargs = self.chain_abi_json_to_bin({
             "code": CONTRACT, "action": "refreshkey",
             "args": {"sym":symbol,"token_id":token_id , "subkey": new_subkey}
@@ -106,7 +106,7 @@ class PCSClient(EosClient):
     def refreshkey2(self,symbol,token_id,new_subkey):
 
         a_day = 24 * 60 * 60
-        message = str(int(datetime.datetime.now().timestamp()/a_day) *a_day* 1000)        
+        message = str(int(datetime.datetime.now().timestamp()/a_day) *a_day* 1000)
         sig = check_sig.sign_message_with_privatekey(self.subprivatekey,message)
 
         binargs = self.chain_abi_json_to_bin({
@@ -120,14 +120,14 @@ class PCSClient(EosClient):
         return self.push_transaction(transaction, chain_id)
 
     def issuetoagent(self,symbol,subkey,memo):
-        
+
         #get_last_id
         last_id = table_client.get_token_lastid(symbol)
         tokenId = last_id +1
-       
+
         #making sig
         a_day = 24 * 60 * 60
-        message = subkey_signature_in_contract("issuetoagent",symbol)     
+        message = subkey_signature_in_contract("issuetoagent",symbol)
         sig = check_sig.sign_message_with_privatekey(self.subprivatekey,message,isbyte=True)
 
         binargs = self.chain_abi_json_to_bin({
@@ -141,21 +141,31 @@ class PCSClient(EosClient):
         ))
         return self.push_transaction(transaction, chain_id)
 
-def subkey_signature_in_contract(action, sym, token_id=False,new_subkey=False):
+def subkey_signature_in_contract(action, sym, token_id=None, new_subkey=None, to=None):
 
-    from check_sig.format import Name,SymbolCode,Uint64,public_key_to_bytes34 
+    from check_sig.format import Name,SymbolCode,Uint64,public_key_to_bytes34
 
-    action_name = Name(action)
-    sym = SymbolCode(sym)
-    timestamp = Uint64(int(datetime.datetime.now().timestamp()) // 15 * 15 * 1000 * 1000)
-    print("timestamp: {}".format(int(timestamp)))
+    act_bin = bytes(Name(action))
+    sym_bin = bytes(SymbolCode(sym))
+    timestamp = int(datetime.datetime.now().timestamp()) // 15 * 15 * 1000 * 1000
+    print("timestamp: {}".format(timestamp))
+    ts_bin = bytes(Uint64(timestamp))
 
-    if action=="refreshkey2":
-        token_id = Uint64(token_id)
-        message = bytes(action_name) + bytes(sym) + bytes(token_id) + public_key_to_bytes34(new_subkey) + bytes(timestamp)
-        print(len(message))
-    elif action=="issuetoagent":
-        message = bytes(action_name) + bytes(sym) + bytes(timestamp)
-        print(len(message)) 
+    if action == "refreshkey2":
+        id_bin = bytes(Uint64(token_id))
+        sk_bin = public_key_to_bytes34(new_subkey)
+        message = act_bin + sym_bin + id_bin + sk_bin + ts_bin
+    elif action == "issuetoagent":
+        message = act_bin + sym_bin + ts_bin
+    elif action == "lock":
+        id_bin = bytes(Uint64(token_id))
+        message = act_bin + sym_bin + id_bin + ts_bin
+    elif action == "transferid2":
+        to_bin = act_bin = bytes(Name(to))
+        id_bin = bytes(Uint64(token_id))
+        message = act_bin + to_bin + sym_bin + id_bin + ts_bin
+    else:
+        message = ""
+
+    print(len(message))
     return message
-    
